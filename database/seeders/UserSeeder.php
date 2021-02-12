76b29ce383f5d206c8d0650576eab8347ad4c403
suainul yaqin <yaqinsuainul1@gmail.com>
2021-02-12 22:37:51 +0700

    seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Barang;
+use App\Models\Barcode;
 use App\Models\Gudang;
+use App\Models\Infra;
+use App\Models\Masuk;
+use App\Models\Mutasi;
+use App\Models\ServiceInfra;
+use App\Models\Suplier;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -22,7 +30,7 @@ class UserSeeder extends Seeder
         Gudang::create([
             'name' => 'gudang A'
         ]);
-        User::create([
+       $h =  User::create([
             'name' => 'HO',
             'email' => 'h@h.com',
             'password' => bcrypt('h'),
@@ -42,7 +50,7 @@ class UserSeeder extends Seeder
             'gudang_id' => 1,
 
         ]);
-        User::create([
+         $k = User::create([
             'name' => 'ketua cabang',
             'email' => 'k@k.com',
             'password' => bcrypt('k'),
@@ -70,5 +78,67 @@ class UserSeeder extends Seeder
             'gudang_id' =>2,
 
         ]);
+        for ($i=0; $i < 5; $i++) { 
+            $g = Gudang::create([
+                'name' => 'gudang B'.$i
+            ]);
+            $s = Suplier::create([
+                'name' => 'suplier '.$i,
+                'alamat' => 'pasuruan',
+                'no_hp' => '093847384'
+            ]);
+            $b = Barang::create([
+                'name' => 'sabun '.$i 
+            ]);
+            $ss = Masuk::create([
+                'suplier_id' => $s->id,
+                'gudang_id' => $g->id,
+                'user_id' => $h->id,
+                'barang_id' => $b->id,
+                'kuantiti' => 5,
+                'harga_satuan' => 12000+($i*100),
+                'kode_akuntan' => 'kukuk'.$i,
+            ]);
+            for ($z = 0; $z < $ss->kuantiti; $z++) {
+                $ss->barcode()->create([
+                    'user_id' => $h->id,
+                    'kode' => mt_rand(10000000, 99999999),
+                ]);
+            }
+        }
+       Barcode::take(5)->update([
+           'status' => 'aktif'
+       ]);
+        $cc = Barcode::where('status','aktif')->get();
+        foreach ($cc as $value) {
+            $value->update([
+                'status' => 'mutasi'
+            ]);
+            Mutasi::create([
+                'user_id' => $h->id,
+                'gudang_id' => 2,
+                'barcode_id' => $value->id,
+                'kode_mutasi' => Str::random(6),
+            ]);
+        }
+        for ($i=0; $i < 10; $i++) { 
+                Infra::create([
+                'user_id' => $k->id,
+                'gudang_id' => $k->id,
+                'name' => 'papan tulis '.$i,
+                'kode' => Str::random(6),
+            ]);
+        }
+        $in = Infra::take(5)->get();
+        foreach ($in as $key => $value) {
+            # code...
+            $value->update([
+                'status' => 'rusak',
+                ]);
+            ServiceInfra::create([
+                'file' => 'https://drive.google.com/file/d/1DL7hrsmdHe4cDydVYhBU72lCFpRL_lFJ/view?usp=sharing',
+                'infra_id' => $value->id
+            ]);
+        }
     }
 }
