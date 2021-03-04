7ffbf34f5d3414bfae9ea70ec03e0621257746ec
pradiknatama <pradiknatama@gmail.com>
2021-03-04 13:26:02 +0700

    sidebar

--- resources/views/frontend/layout-frontend/sidebar.blade.php
+++ resources/views/frontend/layout-frontend/sidebar.blade.php
@@ -12,77 +12,61 @@
           <li class="back-btn"><a href="index.html"><img class="img-fluid" src="../assets/images/logo/logo-icon.png" alt=""></a>
             <div class="mobile-back text-right"><span>Back</span><i class="fa fa-angle-right pl-2" aria-hidden="true"></i></div>
           </li>
-          {{-- update sidebar --}}
-          {{-- @if (auth()->user()->role == 'admin')
-            <li class="dropdown"><a class="nav-link menu-title " href="#"><i data-feather="box"> </i><span>Barang</span></a>
-              <ul class="nav-submenu menu-content">
-                <li><a href="{{route('barang.create')}}">Nama Barang</a></li>
-                <li><a href="{{route('masuk.index')}}">List Barang Masuk</a></li>
-                <li><a href="{{route('barang.index')}}">semua barang</a></li>
-              </ul>
-            </li>
-
-          @endif
-          @if (auth()->user()->role != 'admin')
-          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="box"> </i><span>Kelola Barang</span></a>
+          {{-- all user --}}
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="dashboard"><i data-feather="home"> </i><span>Dashboard</span></a></li>
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="profil"><i data-feather="user"> </i><span>Profil</span></a></li>
+          {{-- end all user --}}
+          {{-- admin --}}
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="profil"><i data-feather="users"> </i><span>User</span></a></li>
+          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="box"></i><span>Barang</span></a>
             <ul class="nav-submenu menu-content">
-              <li><a href="{{route('barang.create')}}">Nama Barang</a></li>
-              @if (auth()->user()->role != 'teknisi')
-              <li><a href="{{route('masuk.index')}}">List Barang Masuk</a></li>
-              @endif
-              @if (auth()->user()->role == 'head')
-              <li><a href="{{route('barang.index')}}">semua barang</a></li>
-
-              @endif
+              <li><a href="user-profile.html">Semua Barang</a></li>
+              <li><a href="edit-profile.html">List Barang Masuk</a></li>
             </ul>
           </li>
-          @endif --}}
-          {{-- end update sidebar --}}
-          <li class="dropdown"><a class="nav-link menu-title" href="dashboard"><i data-feather="home"></i><span>Dashboard</span></a></li>
-          <li class="dropdown"><a class="nav-link menu-title" href="list"><i data-feather="users"></i><span>User</span></a></li>
-          <li class="dropdown"><a class="nav-link menu-title" href="semua"><i data-feather="file-text"></i><span>Semua Barang</span></a></li>
-          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="box"> </i><span>Aktifasi Barcode</span></a></li>
-          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="box"> </i><span>Kelola Barang</span></a>
+          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="settings"></i><span>Pengajuan Service</span></a>
             <ul class="nav-submenu menu-content">
-              <li><a href="{{route('barang.create')}}">Nama Barang</a></li>
-              {{-- @if (auth()->user()->role != 'teknisi') --}}
-              <li><a href="{{route('masuk.index')}}">Barang Masuk</a></li>
-              {{-- @endif --}}
+              <li><a href="user-profile.html">Infrastruktur</a></li>
+              <li><a href="edit-profile.html">Aftersale</a></li>
             </ul>
           </li>
-          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="box"> </i><span>Kelola Mutasi</span></a>
+          {{-- end admin --}}
+          {{--Teknisi  --}}
+          {{-- <h1>teknisi</h1> --}}
+          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="settings"></i><span>Service</span></a>
             <ul class="nav-submenu menu-content">
-              <li><a href="{{route('barang.create')}}">Mutasi</a></li>
-              {{-- @if (auth()->user()->role != 'teknisi') --}}
-              <li><a href="{{route('masuk.index')}}">Riwayat Mutasi</a></li>
-              {{-- @endif --}}
+              <li><a href="user-profile.html">Infrastruktur</a></li>
+              <li><a href="edit-profile.html">Aftersale</a></li>
             </ul>
           </li>
-          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="box"> </i><span>Kelola Infrastruktur</span></a>
+          {{--end teknisi  --}}
+          {{-- HO  --}}
+          {{-- <h1>HO</h1> --}}
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="dashboard"><i data-feather="check-square"> </i><span>Aktivasi Barcode</span></a></li>
+          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="box"></i><span>Barang</span></a>
             <ul class="nav-submenu menu-content">
-              <li><a href="{{route('barang.create')}}">Infrastruktur</a></li>
-              {{-- @if (auth()->user()->role != 'teknisi') --}}
-              <li><a href="{{route('masuk.index')}}">Service Infrastruktur</a></li>
-              {{-- @endif --}}
+              <li><a href="user-profile.html">Nama Barang</a></li>
+              <li><a href="edit-profile.html">List Barang Masuk</a></li>
             </ul>
           </li>
-          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="box"> </i><span>Kelola Aftersale</span></a>
+          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="archive"></i><span>Mutasi</span></a>
             <ul class="nav-submenu menu-content">
-              <li><a href="{{route('barang.create')}}">Aftersale</a></li>
+              <li><a href="user-profile.html">Mutasi</a></li>
+              <li><a href="edit-profile.html">Riwayat Mutasi</a></li>
             </ul>
           </li>
-
-          {{-- teknisi --}}
-          @if (auth()->user()->role == 'teknisi')
-            <li class="dropdown"><a class="nav-link menu-title link-nav" href="{{route('user.index')}}"><i data-feather="users"> </i><span>Profile</span></a></li>
-            <li class="dropdown"><a class="nav-link menu-title" href=""><i data-feather="file-text"></i><span>Infrastruktur</span></a></li>
-            <li class="dropdown"><a class="nav-link menu-title" href=""><i data-feather="box"></i><span>Aftersale</span></a></li>
-          @endif
-          {{-- end teknisi --}}
-          <li class="dropdown"><a class="nav-link menu-title " href="{{route('gudang.index')}}"><i data-feather="monitor"> </i><span>Kelola Gudang</span></a></li>
-          <li class="dropdown"><a class="nav-link menu-title " href="{{route('suplier.index')}}"><i data-feather="monitor"> </i><span>Kelola Suplier</span></a></li>
-
-        </div>
+          <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="menu"></i><span>Infrastruktur</span></a>
+            <ul class="nav-submenu menu-content">
+              <li><a href="user-profile.html">Infrastruktur</a></li>
+              <li><a href="edit-profile.html">Service Infrastruktur</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="dashboard"><i data-feather="file-text"> </i><span>Aftersale</span></a></li>
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="social-app.html"><i data-feather="monitor"> </i><span>Gudang</span></a></li>
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="social-app.html"><i data-feather="list"> </i><span>Suplier</span></a></li>
+          {{--end HO--}}
+        </ul>
+      </div>
       <div class="right-arrow" id="right-arrow"><i data-feather="arrow-right"></i></div>
     </div>
   </nav>
