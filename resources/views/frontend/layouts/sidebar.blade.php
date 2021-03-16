0c2ceee233e6bde16edc6577e5c468e0bd1d86a8
pradiknatama <pradiknatama@gmail.com>
2021-03-16 11:25:16 +0700

    sidebar

--- resources/views/frontend/layouts/sidebar.blade.php
+++ resources/views/frontend/layouts/sidebar.blade.php
@@ -12,7 +12,7 @@
           <li class="back-btn"><a href="index.html"><img class="img-fluid" src="../assets/images/logo/logo-icon.png" alt=""></a>
             <div class="mobile-back text-right"><span>Back</span><i class="fa fa-angle-right pl-2" aria-hidden="true"></i></div>
           </li>
-          
+
           <li class="dropdown"><a class="nav-link menu-title link-nav" href="{{route('home')}}"><i data-feather="home"> </i><span>Dashboard</span></a></li>
           <li class="dropdown"><a class="nav-link menu-title link-nav" href="{{route('user.index')}}"><i data-feather="user"> </i><span>Profile</span></a></li>
           <li class="dropdown"><a class="nav-link menu-title" href="#"><i data-feather="menu"> </i><span>Kelola Infrastruktur</span></a>
@@ -31,14 +31,14 @@
             </ul>
           </li>
           @if (auth()->user()->role != 'teknisi')
-              
+
           @if (auth()->user()->role == 'admin')
-          <li class="dropdown"><a class="nav-link menu-title" href="{{route('user.all')}}"><i data-feather="users"></i><span>User</span></a></li>
-          <li class="dropdown"><a class="nav-link menu-title" href="{{route('barang.index')}}"><i data-feather="file-text"></i><span>Semua Barang</span></a></li>
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="{{route('user.all')}}"><i data-feather="users"></i><span>User</span></a></li>
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="{{route('barang.index')}}"><i data-feather="file-text"></i><span>Semua Barang</span></a></li>
           @endif
-          <li class="dropdown"><a class="nav-link menu-title" href="{{route('barcode.edit')}}"><i data-feather="check-square"> </i><span>Aktifasi Barcode</span></a></li>
-          <li class="dropdown"><a class="nav-link menu-title" href="{{route('barcode.terjual')}}"><i data-feather="box"> </i><span>barang terjual</span></a></li>
-          <li class="dropdown"><a class="nav-link menu-title link-nav" href="#"><i data-feather="box"> </i><span>Kelola Barang</span></a>
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="{{route('barcode.edit')}}"><i data-feather="check-square"> </i><span>Aktifasi Barcode</span></a></li>
+          <li class="dropdown"><a class="nav-link menu-title link-nav" href="{{route('barcode.terjual')}}"><i data-feather="box"> </i><span>barang terjual</span></a></li>
+          <li class="dropdown"><a class="nav-link menu-title " href="#"><i data-feather="box"> </i><span>Kelola Barang</span></a>
             <ul class="nav-submenu menu-content">
               <li><a href="{{route('barang.create')}}">Nama Barang</a></li>
               @if (auth()->user()->role != 'teknisi')
@@ -46,7 +46,7 @@
               @endif
               @if (auth()->user()->role == 'head')
               <li><a href="{{route('barang.index')}}">semua barang</a></li>
-              
+
               @endif
             </ul>
           </li>
