8d86b278ed7132b95e5024cfc7b6c38842e64478
suainul1 <43868229+suainul1@users.noreply.github.com>
2021-04-03 16:17:10 +0700

    Update BarcodeController.php

--- app/Http/Controllers/BarcodeController.php
+++ app/Http/Controllers/BarcodeController.php
@@ -21,7 +21,7 @@ foreach ($barcode as $bk) {
         $bk['bb'] = base64_encode(QrCode::format('svg')->size(100)->errorCorrection('H')->generate($bk->kode));
         }
         $customPaper = array(0,0,567.00,283.80);
-    $pdf = PDF::loadView('backend.barcode',compact('barcode'))->setPaper($customPaper, 'landscape');
+    $pdf = PDF::loadView('backend.barcode',compact('barcode'))->setPaper($customPaper);
     return $pdf->stream();
         return view('backend.barcode',compact('barcode'));
     }
